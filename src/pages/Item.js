ca894ca2c68fd6391d186d4bcd378ae5950ba848
sooraj-gupta <soorajgupta04@gmail.com>
2022-01-24 23:41:41 -0800

    commit

--- src/pages/Item.js
+++ src/pages/Item.js
@@ -4,18 +4,28 @@ import Button from "../components/Button.js";
 import Gallery from "../components/Gallery.js";
 import data from '../data/data.js';
 import Navigation from "./Navigation.js";
+import { GiSpoon } from "react-icons/gi";
+import spoon from "../images/spoon.svg";
 
 function Item( props )
 {
     let params = useParams();
     let item = data.find( item => item.id === params.id );
-    let stars = "★".repeat( item.rating ) + "☆".repeat( 5 - item.rating );
+    let stars = (<></>);
+    for( let i = 0; i < item.rating; i++ )
+    {
+        stars = (<>{stars}<img src = {spoon} width="15"/></> );
+    }
+    for( let i = 0; i < (5 - item.rating); i++ )
+    {
+        stars = (<>{stars}<GiSpoon style = {{color: "#0002"}} /></>);
+    }
     return(
         <>
             <Navigation></Navigation>
             <div className = "section">
                 <div className = "item-header">
-                    <Gallery thumbnail = {item.thumbnail} photos = {item.photos}></Gallery>
+                    <Gallery thumbnail = {item.thumbnail} photos = {item.photos} />
                     <h1>{item.name}</h1>
                     <p className="rating">
                         <span style = {{ 
@@ -23,8 +33,8 @@ function Item( props )
                             fontWeight: '500', 
                             fontSize: '15px',
                             marginRight: '5px'
-
-                        }}>{item.rating}</span> {stars}
+                        }}>{item.rating}</span> 
+                        {stars}
                     </p>
                     <Button>get directions</Button>
                 </div>
