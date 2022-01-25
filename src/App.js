364dd8a58a7b6d13ce70ba741561626bdb070d06
sooraj-gupta <soorajgupta04@gmail.com>
2022-01-25 00:09:03 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as HashRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Item from './pages/Item';
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/item/:id" element = {<Item />}></Route>
